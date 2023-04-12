7ced768980819547d62d0510f9e607c6a7901edb
Gouri-nandana-123 <130470671+Gouri-nandana-123@users.noreply.github.com>
2023-04-12 18:27:44 +0530

    python

---
+++ for.py
@@ -0,0 +1,11 @@
+x=[]
+even=[]
+for i in range(1,100):
+    x.append(i)
+for i in x:
+    if(i%2==0):
+        even.append(i)
+print(even)
+
+
+
---
+++ name.py
@@ -0,0 +1,5 @@
+x=input('enter your name')
+i=1
+while i<=5:
+    print(x)
+    i+=1
